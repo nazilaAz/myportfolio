d1e7548feb7926ff2b755c916c60958f0250a6cb
Nazila <azabdaftar@outlook.com>
2023-03-21 11:45:22 +0100

    contactme finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactmeComponent } from './contactme/contactme.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { MyskillsComponent } from './myskills/myskills.component';
 import { SkillComponent } from './skill/skill.component';
@@ -8,6 +9,7 @@ import { TestComponent } from './test/test.component';
 const routes: Routes = [
   { path: "home", component: HomepageComponent },
   { path: "skill", component: MyskillsComponent },
+  { path: "contact", component: ContactmeComponent },
   { path: "test", component: TestComponent },
   { path: " ", redirectTo: "home" },
   {path:"**",redirectTo:"home"},
--- src/app/contactme/contactme.component.ts
+++ src/app/contactme/contactme.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-contactme',
   templateUrl: './contactme.component.html',
   styleUrls: ['./contactme.component.scss']
 })
-export class ContactmeComponent {
+export class ContactmeComponent implements OnInit {
+  ngOnInit(){
+    $("button").click(function () {
+      $("button").toggleClass("active");
+      $(".title").toggleClass("active");
+      $("nav").toggleClass("active");
+    });
+  }
 
 }
