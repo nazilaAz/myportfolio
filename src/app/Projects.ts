f56898331000c0fe15d0cf27c13eb037574b7305
Nazila <azabdaftar@outlook.com>
2023-05-01 15:27:02 +0200

    new Style with css particles

---
+++ src/app/Projects.ts
@@ -0,0 +1,18 @@
+
+export const frontEndCode=[{
+    "name":"Food-Blog",
+    "img":"foodBlog.png",
+    "description":"Mobile-friendly website for food blogs",
+    "url":"https://nazilaaz.github.io/FE18-CR1-Nazila-AZABDAFTAR/",
+    "tech":"HTML,Scss",
+
+},
+{
+    "name":"My Weekly Planner",
+    "img":"MyWeeklyPlanner.png",
+    "description":"List of Tasks for a “My Weekly Planner”",
+    "url":"https://nazilaaz.github.io/FE18-CR2-Nazila-AZABDAFTAR/",
+    "tech":"HTML,Scss",
+
+}
+]
